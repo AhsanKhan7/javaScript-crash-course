8b109dda7b84f7440e7656c82d1ac21011aeaaf2
AhsanKhan7 <innocent.ahsan12345@gmail.com>
2020-09-22 12:38:51 -0700

    initial commit

---
+++ home.js
@@ -0,0 +1,216 @@
+console.log('hello JS')
+document.write('yeahh')
+
+
+// variables
+
+// var b = 'smoothy'
+// console.log(b)
+
+// var number = 45
+// console.log(number)
+
+// var age = prompt('what is your age ?')
+// document.getElementById('paragraph').innerHTML = age
+
+
+// Numbers
+
+// var num1 = 10
+// var num2 = num1 + 1
+// console.log(num2)
+
+// var num1 = 10
+// num1--
+// console.log(num1)
+
+// var num1 = 10
+// console.log(num1 % 5)
+// console.log(num1 / 2)
+// var num2 = num1 += 10
+// console.log(num2)
+
+// Functions
+
+// function fun() {
+//     alert('this is fumction')
+// }
+
+// fun()
+
+// function greeting(yourName) {
+//     var result = 'hello ' + yourName
+//     console.log(result)
+// }
+// var name = prompt('enter name')
+// greeting(name)
+
+
+// function sumNumber(num1, num2) {
+//     var result = num1 + num2
+//     console.log(result)
+// }
+// sumNumber(10, 5)
+
+
+// While Loops
+
+// var num = 0
+
+// while(num < 100) {
+//     num += 1
+//     console.log(num)
+// }
+
+
+// For loops
+
+// for(let num=0; num <= 100; num++) {
+//     console.log(num)
+// }
+
+
+// Data Types
+
+// let age = 10      //number
+// let name = "ahsan"    //string
+// let obj = { firstName: 'ahsan', lastName: 'naseem'}    //object
+// let arr = ['red', 'green', 'blue']     //array
+// let some =              //undefined
+// let play = true   //boolean
+
+
+// Strings in javascript (common method)
+
+// let fruit = 'banana,orange,pineApple'
+// let fruits = 'apple\nbanana'
+
+// console.log(fruits.length)
+// console.log(fruits.indexOf('s'))
+// console.log(fruit.slice(2, 4))
+// console.log(fruit.replace('ba', 'te'))
+// console.log(fruit.toUpperCase(fruit))
+// console.log(fruit.toLowerCase(fruit))
+// console.log(fruit.charAt(3))
+// console.log(fruit.split(','))
+
+// Array is javascript
+
+// let fruit = ['banana', 'orange', 'pineApple']
+// let fruits = new Array('lim','ore','Apple')
+
+// console.log(fruits[1])
+// fruits[0] = 'watermelon'
+// console.log(fruits)
+
+// for(let i = 0; 1 < fruits.length; i++) {
+//     console.log(fruits[i])
+// }
+
+// Array common method
+
+// console.log('to strings', fruits.toString())
+// console.log(fruits.join('-'))
+// console.log(fruits.pop(), fruits)             // remove last item
+// console.log(fruits.push('blackBerry'), fruits)
+// console.log(fruits[8])
+// fruits[8] = 'new fruit'
+// console.log(fruits)
+// fruits.shift()         // remove first item from list
+// console.log(fruits)
+// fruits.unshift('ddd')
+// console.log(fruits)
+
+
+// let thing = ['juice', 'market', 'movie']
+// let things = fruits.concat(thing)
+// console.log(things.slice(1, 4))
+// console.log(things.reverse())
+
+// let numbers = [3,4,122,44,98,67,9,87,11]
+// console.log(numbers.sort( function(a,b) { return a-b } ))       // sorting in asceding order
+// console.log(numbers.sort( function(a,b) { return b-a } ))       // sorting in desceding order
+
+// let emptyArray = new Array()
+// for (let i=0; i <= 100; i++) {
+//     emptyArray.push(i)
+// }
+// console.log(emptyArray)
+
+
+
+// object in javascript
+
+// let student = {
+//     name: 'ahsan', 
+//     lastName: 'khan', 
+//     age: 22, 
+//     height: 5.8,
+//     studentInfo: function() {
+//         return  'name ' + this.name + '\n' + this.lastName + '\n' + 'age ' + this.age
+//     }
+// }
+
+// console.log(student.name)
+// student.age++
+// console.log(student.studentInfo())
+
+
+// Condition, control flow (if else)
+
+
+// && and
+// || or
+
+// let age = prompt('whats your age')
+
+// if ( (age >= 18 && age <= 35) ) {
+//     status = 'yes'
+// } else {
+//     status = 'no'
+// }
+// console.log(status)
+
+
+// Switch statement
+
+// day 0 = sunday
+// day 1 = monday
+// day 2 = tuesday
+// day 3 = wednesday
+// day 4 = thursday
+// day 5 = friday
+// day 6 = saturday
+
+// switch(0) {
+//     case 0:
+//         text = 'weekend'
+//     case 5:
+//         text= 'weekend'
+//     case 6:
+//         text= 'weekend'
+//         break;
+
+//     default: 
+//         text= 'weekday'
+// }
+// console.log(text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
